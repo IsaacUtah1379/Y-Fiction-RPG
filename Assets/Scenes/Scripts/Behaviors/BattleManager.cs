0a0ad2113698fb5bfeee86e4c5b8dbc19655543c
Isaac E. <122501927+IsaacUtah1379@users.noreply.github.com>
2025-03-30 14:42:31 -0600

    More work on battle management

--- Assets/Scenes/Scripts/Behaviors/BattleManager.cs
+++ Assets/Scenes/Scripts/Behaviors/BattleManager.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+
+public enum BattleState
+{
+    Ongoing,
+    Victory,
+    Defeat
+}
 
 public class BattleManager : MonoBehaviour
 {
-    private MainCharacter[] mainCharacters;
+    private MainCharacter[] activeCharacters;
+    private MainCharacter[] unconsicousCharacters;
     private Enemy[] enemies;
     private IEntity[] initiative;
     private IEntity[] futureInitiative;
@@ -11,12 +21,12 @@ public class BattleManager : MonoBehaviour
 
     public void Start() {
         // TODO: initialize the battle
-        
+        StartCoroutine(DoRound());
     }
 
     private void CalculateInitiative() {
         initiativeChanged = true;
-        futureInitiative = new IEntity[mainCharacters.Length + enemies.Length];
+        futureInitiative = new IEntity[activeCharacters.Length + enemies.Length];
         // TODO: actually calculate futureInitiative
     }
 
@@ -24,13 +34,32 @@ public class BattleManager : MonoBehaviour
         if (initiativeChanged) {
             initiative = futureInitiative;
         }
-        
+
+        BattleState state;
+
         for (int i = 0; i < initiative.Length; i++) {
             yield return StartCoroutine(DoTurn(initiative[i]));
+            state = GetBattleState();
+
+            if (state == BattleState.Defeat || state == BattleState.Victory) {
+                break;
+            }
         }
+
+        // TODO: Handle victory and loss
     }
 
     private IEnumerator DoTurn(IEntity entity) {
         yield return null;
+    }
+
+    private BattleState GetBattleState() {
+        if (enemies.Length == 0) {
+            return BattleState.Victory;
+        } else if (activeCharacters.Length == 0) {
+            return BattleState.Defeat;
+        } else {
+            return BattleState.Ongoing;
+        }
     }
 }
