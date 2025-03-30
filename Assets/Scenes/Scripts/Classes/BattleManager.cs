61bc5207ba372a335b89e4143a7eb0776a92629e
Isaac E. <122501927+IsaacUtah1379@users.noreply.github.com>
2025-03-30 00:29:27 -0600

    Add some info holders and managers

--- Assets/Scenes/Scripts/Classes/BattleManager.cs
+++ Assets/Scenes/Scripts/Classes/BattleManager.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class BattleManager
 {
+    private MainCharacter[] mainCharacters;
+    private Enemy[] enemies;
+    private IEntity[] initiative;
+
     public BattleManager(Enemy[] enemies) {
-        
+        this.mainCharacters = GameObject.Find("Party").GetComponent<PartyInfo>().mainCharacters;
+        this.enemies = enemies;
+    }
+
+    public void Start() {
+
     }
 }
