9728c89a90b9ceadb7031b5133db9c5835a641e4
Isaac E. <122501927+IsaacUtah1379@users.noreply.github.com>
2025-03-30 19:27:29 -0600

    Vector3 to Vector2 conversion

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 
+public enum AxisPair {
+    xy,
+    xz,
+    yz,
+    yx,
+    zx,
+    zy
+}
+
 public static class Utilities
 {
-   
+    public static Vector2 Vector3ToVector2(Vector3 vector, AxisPair pair) {
+        if (pair == AxisPair.xy) {
+            return new Vector2(vector.x, vector.y);
+        } else if (pair == AxisPair.xz) {
+            return new Vector2(vector.x, vector.z);
+        } else if (pair == AxisPair.yz) {
+            return new Vector2(vector.y, vector.z);
+        } else if (pair == AxisPair.yx) {
+            return new Vector2(vector.y, vector.x);
+        } else if (pair == AxisPair.zx) {
+            return new Vector2(vector.z, vector.x);
+        } else if (pair == AxisPair.zy) {
+            return new Vector2(vector.z, vector.y);
+        } else {
+            return Vector2.zero;
+        }
+    }
 }
